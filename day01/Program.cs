dd00764c4b82bfbe19a949ff65adfa36a0359cbe
livia <yamap19950521@gmail.com>
2022-09-29 22:43:31 +0800

    add_C#_01

---
+++ day01/Program.cs
@@ -0,0 +1,24 @@
+﻿//註解
+//這是單行註解
+/*這是
+        雙行
+            註解*/
+
+
+
+//常見資料型態 & 變數
+//字串 string
+System.Console.WriteLine("我是一隻可愛白斬雞");
+
+//字元 char
+System.Console.WriteLine('a');
+
+//整數 int
+System.Console.WriteLine(100);
+
+//浮點數 double
+System.Console.WriteLine(160.5555);
+
+//布林值 bool
+System.Console.WriteLine(true);
+System.Console.WriteLine(false);
